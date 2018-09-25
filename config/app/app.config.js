776aeac2825d0d3cdd3d1aaf38c63307697de2dd
Hristo Oskov <hristo.oskov@gmail.com>
2018-09-25 11:42:40 -0700

    upgrading to babel 7

--- config/app/app.config.js
+++ config/app/app.config.js
@@ -1,21 +1,14 @@
-import moment from 'moment';
-import PACKAGE from '../../package.json';
+const moment = require('moment');
+const PACKAGE = require('../../package.json');
 
 const BANNER = `
 ${PACKAGE.name} - v${PACKAGE.version}
 ${PACKAGE.description}
 ${PACKAGE.homepage}
 
-Copyright (c) 2018-${moment().year()}, OpenLattice, Inc. All rights reserved.
+Copyright (c) 2017-${moment().year()}, OpenLattice, Inc. All rights reserved.
 `;
 
-const APP_INDEX_HTML = 'index.html';
-const APP_JS = 'index.js';
-const APP_CSS = 'index.css';
-
-export default {
+module.exports = {
   BANNER,
-  APP_INDEX_HTML,
-  APP_JS,
-  APP_CSS
 };
