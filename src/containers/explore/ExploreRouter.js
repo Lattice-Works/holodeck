c533c3ebbdc058127ba65e27fc24345200921abd
Hristo Oskov <hristo@openlattice.com>
2019-12-10 17:42:49 -0800

    EntityLinkingContainer doesn't need to exist yet

--- src/containers/explore/ExploreRouter.js
+++ src/containers/explore/ExploreRouter.js
@@ -9,14 +9,12 @@ import { withRouter } from 'react-router-dom';
 
 import ExploreEntitySetContainer from './ExploreEntitySetContainer';
 
-import EntityLinkingContainer from '../linking/EntityLinkingContainer';
 import EntitySetSearch from '../entitysets/EntitySetSearch';
 import * as Routes from '../../core/router/Routes';
 
 const ExploreRouter = () => (
   <Switch>
     <Route exact path={Routes.EXPLORE} component={EntitySetSearch} />
-    <Route path={Routes.ENTITY_LINKING} component={EntityLinkingContainer} />
     <Route path={Routes.EXPLORE_ES} component={ExploreEntitySetContainer} />
     <Redirect to={Routes.EXPLORE} />
   </Switch>
