b9436749bf2d41c42b31b48d8dfdcc7a870f538d
Hristo Oskov <hristo@openlattice.com>
2020-07-02 18:29:28 -0700

    happy linter

--- src/containers/orgs/OrgsRouter.js
+++ src/containers/orgs/OrgsRouter.js
@@ -2,39 +2,29 @@
  * @flow
  */
 
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { Models } from 'lattice';
-import { AppContentWrapper, Spinner } from 'lattice-ui-kit';
-import {
-  RoutingUtils,
-  ValidationUtils,
-  useRequestState,
-} from 'lattice-utils';
-import { useDispatch, useSelector } from 'react-redux';
+import { RoutingUtils } from 'lattice-utils';
+import { useSelector } from 'react-redux';
 import {
   Redirect,
   Route,
   Switch,
   useRouteMatch,
 } from 'react-router';
-import { RequestStates } from 'redux-reqseq';
-import type { RequestState } from 'redux-reqseq';
 
 import OrgContainer from './OrgContainer';
 
-import { ERROR, REDUCERS } from '../../core/redux/constants';
+import { REDUCERS } from '../../core/redux/constants';
 import { Routes } from '../../core/router';
 
 const { Organization } = Models;
 const { getParamFromMatch } = RoutingUtils;
-const { isValidUUID } = ValidationUtils;
 
 const { ORGS } = REDUCERS;
 
 const OrgsRouter = () => {
-
-  const dispatch = useDispatch();
 
   let organizationId :?UUID;
   const matchOrganization = useRouteMatch(Routes.ORGANIZATION);
@@ -44,11 +34,11 @@ const OrgsRouter = () => {
 
   const organization :?Organization = useSelector((s) => s.getIn([ORGS, 'organizationsMap', organizationId]));
 
-  let dataSetId :?UUID;
-  const matchAtlasDataSet = useRouteMatch(Routes.ATLAS_DATA_SET);
-  if (matchAtlasDataSet) {
-    dataSetId = getParamFromMatch(matchAtlasDataSet, Routes.DSID_PARAM);
-  }
+  // let dataSetId :?UUID;
+  // const matchAtlasDataSet = useRouteMatch(Routes.ATLAS_DATA_SET);
+  // if (matchAtlasDataSet) {
+  //   dataSetId = getParamFromMatch(matchAtlasDataSet, Routes.DSID_PARAM);
+  // }
 
   const renderOrgContainer = () => (
     organization && organizationId
